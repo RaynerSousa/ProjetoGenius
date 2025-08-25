3dada6d0b05f9340797370c8a03f2c95f4e1466a
Rayner <raynerwhanyfek@gmail.com>
2025-08-25 10:45:24 -0300

    ajuste no scrol horizontal infinito

--- projetogenius/src/app/linksNavbar/InfinitScroll.tsx
+++ projetogenius/src/app/linksNavbar/InfinitScroll.tsx
@@ -11,18 +11,18 @@ const img = [
 
 export function InfiniteScrollHorizontal() {
   return (
-    <div className="wrapper">
+    <div className="wrapper mt-30">
       <div className="img-item" style={{ "--i": 1 } as React.CSSProperties}>
-        <img src="/computadores.png" alt="" />
+        <img src="/verificacao.jpg  " alt="" />
       </div>
       <div className="img-item" style={{ "--i": 2 } as React.CSSProperties}>
-        <img src="/genius_comp.png" alt="" />
+        <img src="/verificacao.jpg" alt="" />
       </div>
       <div className="img-item" style={{ "--i": 3 } as React.CSSProperties}>
         <img src="/verificacao.jpg" alt="" />
       </div>
       <div className="img-item" style={{ "--i": 4 } as React.CSSProperties}>
-        <img src="/empresarios.jpeg" alt="" />
+        <img src="/verificacao.jpg" alt="" />
       </div>
     </div>
   );
